e84e91293f02b190b634caf3eb93ac7b96ff77a8
Suraj Ghimire <suraj@ghishadow.com>
2022-08-24 08:33:31 +0530

    change font size for gui
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -35,7 +35,7 @@ local options = {
   wrap = false, -- display lines as one long line
   scrolloff = 8, -- is one of my fav
   sidescrolloff = 8,
-  guifont = "Victor Mono::h14",
+  guifont = "Victor Mono::h10",
 }
 
 
