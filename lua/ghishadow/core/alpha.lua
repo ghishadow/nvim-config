01f48930b21b7845331180d468deff4f2eb58cc5
Suraj Ghimire <suraj@ghishadow.com>
2023-07-05 20:26:40 +0530

    fix icons

--- lua/ghishadow/core/alpha.lua
+++ lua/ghishadow/core/alpha.lua
@@ -13,13 +13,12 @@ dashboard.section.header.val = {
 	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("f", " Find file", ":Telescope find_files <CR>"),
 	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
 	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("t", "T Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("c", "  Settings", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit", ":qa<CR>"),
+	dashboard.button("q", "  Quit", ":qa<CR>"),
 }
 
 local function footer()
