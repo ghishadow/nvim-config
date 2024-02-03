30c3c3920a91b26ccc88e458e30fa6aa8031fe3e
Suraj Ghimire <suraj@ghishadow.com>
2024-02-03 21:24:51 +0530

    switch to monaspace
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- lua/ghishadow/core/colorscheme.lua
+++ lua/ghishadow/core/colorscheme.lua
@@ -1,4 +1,4 @@
-local colorscheme = "flexoki"
+local colorscheme = "flexoki-light"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- lua/ghishadow/core/options.lua
+++ lua/ghishadow/core/options.lua
@@ -34,7 +34,7 @@ local options = {
   linebreak = true,                        -- companion to wrap, don't split words
   scrolloff = 8, -- is one of my fav
   sidescrolloff = 8,
-  guifont = "JetBrainsMono Nerd Font::h12",
+  guifont = "Monaspace::h12",
   whichwrap = "bs<>[]hl",                  -- which "horizontal" keys are allowed to travel to prev/next line
 }
 
